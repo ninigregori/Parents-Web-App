8e8ca662cc208759d74257931954fb5b0e012372
Nini Gregori <nini.gregori@gmail.com>
2016-08-10 17:01:17 +0100

    main js
    first login steps

---
+++ js/main.js
@@ -0,0 +1,69 @@
+// Login Controller
+var app = angular.module('LoginController', [] );
+.app.controller( 'LoginController', function ($scope, $rootScope, AUTH_EVENTS, AuthService) {
+  $scope.credentials = {
+    username: '',
+    password: ''
+  };
+  $scope.login = function (credentials) {
+    AuthService.login(credentials).then(function (user) {
+      $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+      $scope.setCurrentUser(user);
+    }, function () {
+      $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+    });
+  };
+})
+
+// Session changes
+.constant('AUTH_EVENTS', {
+  loginSuccess: 'auth-login-success',
+  loginFailed: 'auth-login-failed',
+  logoutSuccess: 'auth-logout-success',
+  sessionTimeout: 'auth-session-timeout',
+  notAuthenticated: 'auth-not-authenticated',
+  notAuthorized: 'auth-not-authorized'
+})
+
+// Access Control (Authentication Services)
+.factory('AuthService', function ($http, Session) {
+  var authService = {};
+
+  authService.login = function (credentials) {
+    return $http
+      .post('/login', credentials)
+      .then(function (res) {
+        Session.create(res.data.id, res.data.user.id,
+                       res.data.user.role);
+        return res.data.user;
+      });
+  };
+
+  authService.isAuthenticated = function () {
+    return !!Session.userId;
+  };
+
+  authService.isAuthorized = function (authorizedRoles) {
+    if (!angular.isArray(authorizedRoles)) {
+      authorizedRoles = [authorizedRoles];
+    }
+    return (authService.isAuthenticated() &&
+      authorizedRoles.indexOf(Session.userRole) !== -1);
+  };
+
+  return authService;
+})
+
+// Current User
+.controller('ApplicationController', function (
+  $scope,
+  USER_ROLES,
+  AuthService) {
+    $scope.currentUser = null;
+    $scope.userRoles = USER_ROLES;
+    $scope.isAuthorized = AuthService.isAuthorized;
+
+    $scope.setCurrentUser = function (user) {
+    $scope.currentUser = user;
+  };
+});
