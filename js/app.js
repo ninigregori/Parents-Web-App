346e658ace5c82735bad419468df6062818303dd
Nini Gregori <nini.gregori@gmail.com>
2016-08-17 16:51:38 +0100

    sign $scope.user

--- js/app.js
+++ js/app.js
@@ -17,37 +17,19 @@ app.controller('Main', function( $scope, $http ) {
 //cache user id from url
 
     //blank object to handle form data
-  //  $scope.user = {};
+    $scope.user = {};
      // calling our submit function
-    $scope.submitForm = function( $http ) {
+    $scope.submitForm = function() {
        // Adding data to api and send itback to server
       return $http.post({
          method  : 'POST',
          url     : 'http://ec2-54-152-102-95.compute-1.amazonaws.com/playbrush/api/v1.0/add_user_to_profile' + 'fname' + 'lname' + 'email',
          data    : $scope.user,
          headers : {'JSON.stringify': 'playbrush_id: pb_id, fname: main.fname, lname: main.lname, email: main.email'}
-
         });
-        success(function(data, status, header, config) {
-                        console.log(user);
-                      })
+         console.log(submitForm);
       }
 });
-
-
-//       .success(function(data) {
-//         if (data.errors) {
-//           // Showing errors.
-//           $scope.errorName = data.errors.name;
-//           $scope.errorUserName = data.errors.username;
-//           $scope.errorEmail = data.errors.email;
-//         } else {
-//           $scope.message = data.message;
-//         }
-//       });
-//     };
-// });
-
 
 // Verify user in data base
 // http://ec2-54-88-245-245.compute-1.amazonaws.com/playbrush/api/v1.0/authenticate/id/<ob_id>
